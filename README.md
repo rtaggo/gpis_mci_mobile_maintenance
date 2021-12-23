2e3fad326f1474462cbaf64c1c754cb856cfbe2c
rtaggo <16922850+rtaggo@users.noreply.github.com>
2021-12-23 16:08:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# GPIS MCI Mobile App
+
+GPIS MCI Mobile Node.js app using [Express 4](http://expressjs.com/).
+
+## Clone the repository
+```sh
+$ git clone https://github.com/rtaggo/gpis_mci_mobile.git
+```
+Once the project is fetched, 
+```sh
+$ cd gpis_mci_mobile
+```
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) 
+
+```sh
+$ npm install
+$ npm start
+```
+or
+
+using nodemon to restart the server on the fly in dev
+```sh
+$ npm install
+$ npm run dev
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+toto
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.disable('etag');
+
+/* Try 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+*/
+
+// [END enable_parser]
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/maintenance.html'));
+});
+
+// Basic 404 handler
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use((err, req, res) => {
+  /* jshint unused:false */
+  console.error(err);
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
